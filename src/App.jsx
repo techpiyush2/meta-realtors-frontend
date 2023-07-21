387d0f7c0dda3924e9faf44523abdacf60d37407
Piyush <pk409678@gmail.com>
2023-07-21 08:22:45 +0530

    routing fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { Routes, Route } from 'react-router-dom';
+// import { useSelector } from "react-redux";
 
 import HomePage from "./pages/HomePage";
 import Listings from "./pages/Listings";
@@ -12,46 +12,32 @@ import Agent from "./pages/Agent";
 import Layout from "./components/Layout/Layout";
 
 function App() {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   return (
     <Layout>
-      {/* <BrowserRouter>
-      <Routes> */}
+      <Routes>
         
-        {/* <Route path="/home" element={<HomePage/>}/> */}
-        <HomePage/>
-        {/* <Routes>
+        <Route path="/home" element={<HomePage/>}/>
 
         <Route path="/listings" exact element={<Listings />}/>
-        <Route path=":listingId" element={<ListingDetail />} />
-        </Routes>
+        <Route path="/listings/:listingId" element={<ListingDetail />} />
         
         <Route path="/agent" element={<Agent />}/>
         
-        <Routes>
         <Route path="/blog" exact element={ <Blog />}/>
-        <Route path=":blogId" element={ <Blog />}/>
-        </Routes>
+        <Route path="/blog/:blogId" element={ <Blog />}/>
   
         
-        <Routes>
+  
         <Route path="/signup" exact element={ <Signup />}/>
           
-          {!isAuthenticated && <Signup />}
-          {isAuthenticated && <Redirect to="/" />}
-        </Routes>
-        
-        <Routes>
+       
         <Route path="/login" exact element={ <Login />}/>
-          
-          {!isAuthenticated && <Login />}
-          {isAuthenticated && <Redirect to="/" />}
-        </Routes> */}
+
         
-        {/* <Route path="*"  element= {<NotFound/>}/>
+     <Route path="*"  element= {<NotFound/>}/>
         </Routes>
-      </BrowserRouter> */}
     </Layout>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
@@ -10,8 +9,8 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </Provider>
 );
