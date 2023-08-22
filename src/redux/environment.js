e496d6af9a84dc508cb9b145bd9d6ea1bed70b07
piyush <piyush@gmail.com>
2023-08-22 17:16:04 +0530

    brands img fix

--- src/redux/environment.js
+++ src/redux/environment.js
@@ -1,4 +1,4 @@
 export default {
-  // baseUrl: 'https://metarealtor.onrender.com/',
-  baseUrl: 'http://192.168.1.9:5000/',
+  baseUrl: 'https://metarealtor.onrender.com/',
+  // baseUrl: 'http://192.168.1.9:5000/',
 }
